1f2575a4c28ee25f751f0a7df548b25a096eaf4b
darkero1 <thedark76m@gmail.com>
2023-11-23 23:19:31 +0300

    updating security

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    ssl: { rejectUnauthorized: false},
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     host: process.env.DATABASE_HOST,
     port: 5432,
     user: process.env.DATABASE_USER,

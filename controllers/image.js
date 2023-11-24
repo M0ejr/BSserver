0d859cae582eb11a5bfd81def11eb6a2badc8427
darkero1 <thedark76m@gmail.com>
2023-11-24 20:44:41 +0300

    updating a few things

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,6 @@ const returnClarifaiRequestOptions = (imageUrl) => {
   const PAT = "1688182ed34d4c109fd1258085461dbe";
   const USER_ID = "moejr";
   const APP_ID = "smartbrain";
-  // eslint-disable-next-line
   const MODEL_ID = "face-detection";
   const IMAGE_URL = imageUrl;
 
@@ -35,33 +34,21 @@ const returnClarifaiRequestOptions = (imageUrl) => {
 };
 
 const handleApiCall = (req, res) => {
-  const clarifaiOptions = returnClarifaiRequestOptions(req.body.input);
+  const requestOptions = returnClarifaiRequestOptions(req.body.input);
 
-  fetch("https://api.clarifai.com/v2/models/face-detection/outputs", clarifaiOptions)
-    .then((response) => {
-      console.log('Clarifai API Response Status:', response.status);
-      // Print response body for additional details
-      return response.text();
-    })
+  fetch(
+    "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+    requestOptions
+  )
+    .then((response) => response.json())
     .then((data) => {
-      console.log('Clarifai API Response Data:', data);
-      // Attempt to parse response body as JSON
-      try {
-        const jsonData = JSON.parse(data);
-        res.json(jsonData);
-      } catch (parseError) {
-        // If parsing fails, send raw response text
-        res.status(400).send(data);
-      }
+      res.json(data);
     })
     .catch((err) => {
-      console.error('Error in handleApiCall:', err);
-      res.status(400).json('Unable to work with API');
+      console.error("Error calling Clarifai API:", err);
+      res.status(400).json("Unable to work with API");
     });
 };
-
-
-
 
 // Your handleImage function
 const handleImage = (req, res, db) => {

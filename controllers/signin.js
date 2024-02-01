c3300bbaea6afdc768b1b4ee47ccc14e2184a5c8
Moejr <mo3jrr@gmail.com>
2024-02-01 21:01:19 +0300

    Error Handling

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,7 +7,6 @@ const redisClient = redis.createClient({
   legacyMode: true
 });
 
-
 redisClient.on('connect', () => {
   console.log('Connected to Redis');
 });
@@ -17,25 +16,33 @@ redisClient.on('error', (err) => {
   console.error(`REDIS_URL: ${process.env.REDIS_EXTERNAL_URL}`);
 });
 
-
-
 const signToken = (username) => {
   const jwtPayload = { username };
-  return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days'});
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' });
 };
 
-const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+const setToken = (key, value) => {
+  return new Promise((resolve, reject) => {
+    redisClient.set(key, value, (err, reply) => {
+      if (err) {
+        console.error(`Error setting token in Redis: ${err}`);
+        reject(err);
+      } else {
+        resolve(reply);
+      }
+    });
+  });
+};
 
 const createSession = (user) => {
   const { email, id } = user;
   const token = signToken(email);
   return setToken(token, id)
     .then(() => {
-      return { success: 'true', userId: id, token, user }
+      return { success: 'true', userId: id, token, user };
     })
     .catch(console.log);
 };
-
 
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
@@ -65,22 +72,20 @@ const getAuthTokenId = (req, res) => {
   const { authorization } = req.headers;
   return redisClient.get(authorization, (err, reply) => {
     if (err || !reply) {
-      return res.status(401).send('Unauthorized')
+      return res.status(401).send('Unauthorized');
     }
     return res.json({ id: reply });
   });
 };
 
-
-
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization ? getAuthTokenId(req, res)
     : handleSignin(db, bcrypt, req, res)
-    .then(data =>
-      data.id && data.email ? createSession(data) : Promise.reject(data))
-    .then(session => res.json(session))
-    .catch(err => res.status(400).json(err));
-}
+      .then(data =>
+        data.id && data.email ? createSession(data) : Promise.reject(data))
+      .then(session => res.json(session))
+      .catch(err => res.status(400).json(err));
+};
 
 export { handleSignin, signinAuthentication, redisClient };
